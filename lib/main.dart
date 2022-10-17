8624062d1a1219cdc3a6a4cec94124bdb6c26d9b
Dhani AM <dhani.a.mm@gmail.com>
2022-10-17 12:24:17 +0800

    added getX dependecy, added getX to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'main_app.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   /// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Locality',
       theme: ThemeData(
         fontFamily: 'comfortaa',
